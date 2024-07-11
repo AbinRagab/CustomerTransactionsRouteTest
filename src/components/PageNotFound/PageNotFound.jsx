a1ba956da2fe62cab30f000df42e5cb5c6683275
AbinRagab <ahmderagabe@gmail.com>
2024-07-11 15:51:50 +0300

    Update The Routing 2

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function PageNotFound() {
+  const navto = useNavigate()
+
+  useEffect(function(){
+    navto('/customer')
+  },[])
   return (
     <div>PageNotFound</div>
   )
